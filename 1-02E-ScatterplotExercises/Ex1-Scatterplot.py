830f8a6b7f69cf2b80bc8c34dde8fa5b9d36ed1b
matt <matt.palmer@storyblocks.com>
2021-02-03 11:06:36 -0500

    text

--- 1-02E-ScatterplotExercises/Ex1-Scatterplot.py
+++ 1-02E-ScatterplotExercises/Ex1-Scatterplot.py
@@ -7,21 +7,35 @@
 ######
 
 # Perform imports here:
-
-
+from plotly.graph_objs.graph_objs import Layout
+import plotly.offline as pyo
+import plotly.graph_objs as go
+import numpy as np
 
 
 # Define a data variable
 
+np.random.seed(43)
+random_x = np.random.randn(1000)
+random_y = np.random.rand(1000)
 
-
-
+data = [go.Scatter(
+    x=random_x
+    , y=random_y
+    , mode='markers'
+)]
 
 
 # Define the layout
 
-
-
-
+layout = go.Layout(
+    title='scatter exercise'
+    , xaxis=dict(title='random ints')
+    , yaxis=dict(title='random floats')
+    , hovermode='closest'
+)
 
 # Create a fig from data and layout, and plot the fig
+
+fig = go.Figure(data=data, layout=layout)
+pyo.plot(fig, filename='scatter_ex.html')
